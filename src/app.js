9911257ad886e432b772324f06b5fd4bf633738d
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2020-12-11 08:52:45 -0600

    fix: remove cors from app

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ const corsNotEnabledRouter = require("./cors-not-enabled/cors-not-enabled.router
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
-app.use(require("cors")());
-
 app.use(express.json());
 
 app.use("/cors-enabled", corsEnabledRouter);
