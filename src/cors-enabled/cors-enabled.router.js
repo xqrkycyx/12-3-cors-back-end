92dda328a98ed9499d28ee44b1d2cd39a2cd5c37
Will Williams <wwilliams@indeed.com>
2023-12-23 12:21:44 -0600

    Enable CORS for single endpoint (DELETE)

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,17 +1,21 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+const corsDelete = cors({ methods: "DELETE" });
 
 router
   .route("/:corsId")
   .get(controller.read)
   .put(controller.update)
-  .delete(controller.delete)
+  .delete(corsDelete, controller.delete)
+  .options(corsDelete)
   .all(methodNotAllowed);
 
 router
   .route("/")
-  .get(controller.list)
+  .get(cors(), controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
 
